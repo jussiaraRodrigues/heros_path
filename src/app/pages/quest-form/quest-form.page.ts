6849e34e9a526ef6636277aed3df8ec3bdb51a19
jussiaraRodrigues <163759923+jussiaraRodrigues@users.noreply.github.com>
2025-09-14 19:35:09 -0300

    ... tentativas de menu de icone

--- src/app/pages/quest-form/quest-form.page.ts
+++ src/app/pages/quest-form/quest-form.page.ts
@@ -1,32 +1,36 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonItem, IonInput, ModalController } from '@ionic/angular/standalone';
+import { IonLabel, IonSelect, IonSelectOption, IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonItem, IonInput, ModalController } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { closeCircleOutline, checkmarkCircleOutline } from 'ionicons/icons';
+import {  closeCircleOutline, checkmarkCircleOutline, bookOutline, barbellOutline, codeWorkingOutline, laptopOutline, walkOutline, waterOutline, leafOutline, bedOutline  } from 'ionicons/icons';
 import { Quest } from '../../services/firebase';
 
 @Component({
-  selector: 'app-quest-form',      // <- CORRIGIDO
-  templateUrl: './quest-form.page.html', // <- CORRIGIDO
-  styleUrls: ['./quest-form.page.scss'],   // <- CORRIGIDO
+  selector: 'app-quest-form',     
+  templateUrl: './quest-form.page.html', 
+  styleUrls: ['./quest-form.page.scss'],  
   standalone: true,
-  imports: [ IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonIcon, IonItem, IonInput ]})
+  imports: [ IonLabel,IonSelect, IonSelectOption, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonIcon, IonItem, IonInput ]})
 
   export class QuestFormPage implements OnInit { 
 
     @Input() questToEdit?: Quest;
 
   private modalCtrl = inject(ModalController);
-  questName: string = ''; 
+  questName: string = '';
+  selectedIcon: string = 'book-outline';
+
+     availableIcons = [ 'book-outline', 'barbell-outline', 'code-working-outline', 'laptop-outline', 'walk-outline', 'water-outline', 'leaf-outline', 'bed-outline' ];
 
   constructor() {
-    addIcons({ closeCircleOutline, checkmarkCircleOutline });
+    addIcons({  closeCircleOutline, checkmarkCircleOutline, bookOutline, barbellOutline, codeWorkingOutline, laptopOutline, walkOutline, waterOutline, leafOutline, bedOutline  });
   }
 
   ngOnInit() {
     if (this.questToEdit) {
       this.questName = this.questToEdit.nome;
+      this.selectedIcon = this.questToEdit.icone;
     }
   }
 
@@ -35,6 +39,10 @@ import { Quest } from '../../services/firebase';
   }
 
   confirm() {
+    const questData = {
+      nome: this.questName,
+      icone: this.selectedIcon
+    };
     return this.modalCtrl.dismiss(this.questName, 'confirm');
   }
 }
